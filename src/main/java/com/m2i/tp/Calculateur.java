d905faac517f2bf9b0285ebfd667301113a49b73
didier <didier@ici_ou_labas.fr>
2018-05-29 10:55:41 +0200

    version initiale

---
+++ src/main/java/com/m2i/tp/Calculateur.java
@@ -0,0 +1,11 @@
+package com.m2i.tp;
+
+public class Calculateur {
+	public 	double tva(double ht, double tauxTvaEnPct){
+		return 0.0;
+		}
+	
+	public 	double ttc(double ht, double tauxTvaEnPct){
+		return 0.0;
+	}
+}
---
+++ src/test/java/com/m2i/test/TestCalculateur.java
@@ -0,0 +1,13 @@
+package com.m2i.test;
+
+public class TestCalculateur {
+	
+	/*
+	 * 1. ecrire les tests via JUnit 4 avec @Before , @Test , Assert.assertEquals()
+	 * 2. lancer les tests avant de finir la programmation du composant
+	 *    pour visualiser rouge (echecs bien détectés)
+	 * 3. écrire le code complet du composant à tester
+	 * 4. relancer les tests qui devraient réussir
+	 */
+
+}
