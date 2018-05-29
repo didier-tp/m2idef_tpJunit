67fef9498f5fa4b6ef57027107069fb8696ab3c6
didier <didier@ici_ou_labas.fr>
2018-05-29 11:18:59 +0200

    nouvelle version

--- src/test/java/com/m2i/test/TestCalculateur.java
+++ src/test/java/com/m2i/test/TestCalculateur.java
@@ -1,6 +1,27 @@
 package com.m2i.test;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import com.m2i.tp.Calculateur;
+
 public class TestCalculateur {
+	private Calculateur calculateur; //à tester
+	
+	@Before
+	public void init(){
+		calculateur = new Calculateur();
+	}
+	
+	@Test
+	public void testTva(){
+		Assert.assertEquals(40, calculateur.tva(200.0, 20.0),0.0001);
+	}
+	
+	@Test
+	public void testTtc(){
+		Assert.assertEquals(240, calculateur.tva(200.0, 20.0),0.0001);
+	}
 	
 	/*
 	 * 1. ecrire les tests via JUnit 4 avec @Before , @Test , Assert.assertEquals()
