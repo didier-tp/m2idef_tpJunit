0650b74c1a4399ebef8084a9fdf2e3e4091ab9f0
didier <didier@ici_ou_labas.fr>
2018-05-29 11:58:58 +0200

    nouvelle version

--- src/main/java/com/m2i/tp/Calculateur.java
+++ src/main/java/com/m2i/tp/Calculateur.java
@@ -1,7 +1,13 @@
 package com.m2i.tp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Calculateur {
+	private static Logger logger = LoggerFactory.getLogger(Calculateur.class);
+	
 	public 	double tva(double ht, double tauxTvaEnPct){
+		logger.trace("tva() appele sur Calculateur avec ht=" + ht + ", taux = " + tauxTvaEnPct);
 		return ht * tauxTvaEnPct / 100;
 		}
 	
